d9d67c2725da45ff630e6777063a03841c6e68f8
G14p <116430098+Giapppp@users.noreply.github.com>
2023-02-05 12:27:00 +0700

    Create MOV_Attack.py

---
+++ Useful_Thing/Elliptic Curve/MOV_Attack.py
@@ -0,0 +1,24 @@
+"""
+When the embedding degree k is small, we can use MOV Attack to transfer the discrete log from E(F_p) to F_p^k, which is easier to solve:
+
+Resource: https://crypto.stanford.edu/pbc/notes/elliptic/movattack.html
+"""
+
+#Code: From ConnerM
+#----------------------------------------------------------------------------#
+#Check the embedding 
+k = 1
+while (p**k - 1) % E.order() != 0:
+    k += 1
+
+#MOV attack !!!
+def MOV_attack(E, G, A, k):
+    E2 = EllipticCurve(GF(p**k), [a,b])
+    T = E2.random_point()
+    M = T.order()
+    N = G.order()
+    T1 = (M//gcd(M, N)) * T
+    _G = E2(G).weil_pairing(T1, N)
+    _A = E2(A).weil_pairing(T1, N)
+    nA = _A.log(_G)
+    return nA
