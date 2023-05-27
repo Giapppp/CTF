c7548b59835755c6b0c45bee3861f0db469dea4c
G14p <116430098+Giapppp@users.noreply.github.com>
2023-05-27 08:27:09 +0700

    Update smooth_prime_generator.py

--- Useful_Thing/smooth_prime_generator.py
+++ Useful_Thing/smooth_prime_generator.py
@@ -1,13 +1,25 @@
 #When we need to solve a discrete log problem and can fix the prime, a smooth prime will be a better choice
 
-from Crypto.Util.number import isPrime
+from Crypto.Util.number import *
+import gmpy2
+import random
+original_P = getPrime(1024)
 
-def smooth_prime(size):
-    i = 2
-    smooth_p = 1
-    while smooth_p < size or not isPrime(smooth_p + 1):
-        smooth_p *= i
-        i += 1
-    smooth_p += 1
-    return smooth_p
-  
+primes = [2]
+for i in range(1000):
+    primes.append(int(gmpy2.next_prime(primes[-1])))
+primes=primes[100:]  #keep just big primes
+
+# generate a weak prime (P) such that P>original_P
+while True:
+    N = 2
+    factors=[]
+    while (N<original_P):  
+        prime=random.choice(primes)
+        if prime not in factors:
+            factors.append(prime)
+            N*=prime
+    if gmpy2.is_prime(N+1):
+        break
+P=N+1  
+print(P)
