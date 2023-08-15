c77690569f1916255518608b82bea7f4cb28a4c4
G14p <116430098+Giapppp@users.noreply.github.com>
2023-08-16 03:39:23 +0700

    Update flatter.py

--- Useful_Thing/flatter.py
+++ Useful_Thing/flatter.py
@@ -1,6 +1,9 @@
 #When reduced very big matrix (about 512*512), flatter is a very cool tool to use
 #Source: @maple3142
 
+from subprocess import check_output
+from re import findall
+
 def flatter(M):
     # compile https://github.com/keeganryan/flatter and put it in $PATH
     z = "[[" + "]\n[".join(" ".join(map(str, row)) for row in M) + "]]"
